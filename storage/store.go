3df1babf4bd817c0293efbf6b6fb446e73e53f8e
Rafael Portela <rafael@cheesy.dev>
2021-04-05 20:11:06 +0200

    Remove unused err

--- storage/store.go
+++ storage/store.go
@@ -2,13 +2,10 @@ package storage
 
 import (
 	"embed"
-	"errors"
 )
 
 //go:embed files/*
 var files embed.FS
-
-var ErrNotFound = errors.New("File not found")
 
 func Find(filename string) (string, error) {
 	content, err := files.ReadFile("files/" + filename)
